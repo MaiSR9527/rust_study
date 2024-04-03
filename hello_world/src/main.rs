f06dfd67cd69ba58c88957767808adb65060c1da
maishuren <maisrcn@qq.com>
2024-04-03 19:31:06 +0800

    update

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,11 @@
+fn greet_world() {
+    let chinese = "你好，世界！";
+    let english = "hello, world!";
+    let languages = [chinese, english];
+    for lang in languages.iter() {
+        println!("{}", &lang);
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    greet_world();
 }
